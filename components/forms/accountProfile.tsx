fc49a006480e4b4d9329649e7e4f439d108b9d3b
Clupai8o0 <clupaio4@gmail.com>
2023-08-07 10:34:27 +0300

    lots done now backend finna bouta start

--- components/forms/accountProfile.tsx
+++ components/forms/accountProfile.tsx
@@ -20,6 +20,8 @@ import { Input } from "@/components/ui/input";
 import { UserValidation } from "@/lib/validations/user";
 import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
 	user: {
@@ -35,6 +37,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
 	const [files, setFiles] = useState<File[]>([]);
+	const { startUpload } = useUploadThing("media");
 
 	const form = useForm({
 		resolver: zodResolver(UserValidation),
@@ -67,8 +70,19 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 		}
 	};
 
-	function onSubmit(values: z.infer<typeof UserValidation>) {
-		console.log(values);
+	async function onSubmit(values: z.infer<typeof UserValidation>) {
+		const blob = values.profile_photo;
+		const hasImageChanged = isBase64Image(blob);
+
+		if (hasImageChanged) {
+			const imgRes = await startUpload(files);
+
+			if (imgRes && imgRes[0].fileUrl) {
+				values.profile_photo = imgRes[0].fileUrl;
+			}
+		}
+
+		// todo: update user profile
 	}
 
 	return (
